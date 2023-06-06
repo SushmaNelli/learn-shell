129b14e18ddf970aa8b9bef6a1391429497978c5
Sushma <nellisushma@gmail.com>
2023-06-05 22:59:02 -0500

    first shell script

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,11 @@
 echo hello world
+
+#to print in diff. colors we use
+#syntax : echo -e "\e[COLmMESSAGE\e[0m"
+
+echo -e "\e[34mSushma\e[0m"
+echo -e "\e[32mSushma\e[0m"
+echo -e "\e[36mSushma\e[0m"
+echo -e "\e[31mSushma\e[0m"
+echo -e "\e[33mSushma\e[0m"
+echo -e "\e[35mSushma\e[0m"
