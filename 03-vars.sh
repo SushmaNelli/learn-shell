6e05545fdf89b6532485828cc6a1ce0c5d3952da
Sushma <nellisushma@gmail.com>
2023-08-10 18:24:10 -0500

    variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -3,6 +3,6 @@ a=10
 echo value of a is $adollars
 
 # alternatively variables also can be accessed by using ${a}
-echo value of a is ${a}dollars
+echo value of a is${a}dollars
 
 echo value of a is $a dollars
