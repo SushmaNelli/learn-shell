4865f044e82aa8d16203b1f8f1d599a28b6a08e0
Sushma <nellisushma@gmail.com>
2023-08-17 18:19:30 -0500

    functinos

--- 04.functions.sh
+++ 04.functions.sh
@@ -9,3 +9,12 @@ print_a() {
 print_hello
 
 print_a
+
+print-arguments() {
+  echo first argument $1
+  echo second argument $2
+  echo all arguments $*
+  echo No. of arguments $#
+}
+
+print_arguments abc 123 xyz
