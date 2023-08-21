5645a32d85fda5dd06c16a3354ab7b8c48ba41b8
Sushma <nellisushma@gmail.com>
2023-08-21 16:52:33 -0500

    functinos

--- 06.loops.sh
+++ 06.loops.sh
@@ -5,3 +5,8 @@ while [ $a -gt 0 ]; do
   a=$(($a-1))
   sleep 1
   done
+
+  for component in catalogue frontend cart user payment shipping ; do
+    echo Creating component - $component
+    sleep 1
+    done
